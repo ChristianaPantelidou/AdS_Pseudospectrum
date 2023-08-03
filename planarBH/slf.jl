1b9b3d1e1cc6ea6ef4526a573a213f5e9750be22
bradcownden <cowndenb@gmail.com>
2023-08-03 17:40:32 +0100

    Update slf.jl
    Corrections to Sturm-Louiville functions

--- planarBH/slf.jl
+++ planarBH/slf.jl
@@ -12,7 +12,7 @@ using ThreadsX
 export w, p, pp, V, gamma, gammap
 
     function f_p(x::Array)
-        rho = -x ./2 .+ (1/2)
+        rho = x ./2 .+ (1/2)
         foo = Vector{eltype(x)}(undef, length(x))
         ThreadsX.map!(i -> 1 - (1 - rho[i])^4, foo, eachindex(rho))
         return foo
@@ -23,33 +23,35 @@ export w, p, pp, V, gamma, gammap
     end
 
     function p(x::Array)
-        rho = -x ./ 2 .+ (1/2)
-        f = f_p(x)
-        foo = Vector{eltype(x)}(undef, length(rho))
-        ThreadsX.map!(i -> (2 * f[i] - 3) / (1 - rho[i]) - (1 - rho[i]), foo, eachindex(rho))
-        return foo
-    end
-    
-    function pp(x::Array)
         return -f_p(x)
     end
     
+    function pp(x::Array)
+        rho = x ./ 2 .+ (1/2)
+        f = f_p(x)
+        foo = Vector{eltype(x)}(undef, length(rho))
+        ThreadsX.map!(i -> (f[i] / (1 - rho[i])) - 4 * (1 - rho[i])^3, foo, eachindex(rho))
+        return foo
+    end
+    
     function V(x::Array, m::Float64, q::Float64)
-        rho = -x ./ 2 .+ (1/2)
+        rho = x ./ 2 .+ (1/2)
         f = f_p(x)
         foo = Vector{eltype(x)}(undef, length(x))
-        ThreadsX.map!(i -> (m^2 - 2 * f[i] + 6) / (1 - rho[i])^2 + q^2 + 2 * (1 - rho[i])^2 , foo, eachindex(x))
+        ThreadsX.map!(i -> (2 * f[i] - m^2 - 6) / (1 - rho[i])^2 - q^2 
+            - 2 * (1 - rho[i])^2 , foo, eachindex(x))
         return foo
     end
 
     function gamma(x::Array)
-        return 2 .* (f_p(x) .- 1)
+        return f_p(x) .- 1
     end
 
     function gammap(x::Array)
-        rho = -x ./ 2 .+ (1/2)
+        rho = x ./ 2 .+ (1/2)
+        f = f_p(x)
         foo = Vector{eltype(x)}(undef, length(x))
-        ThreadsX.map!(i -> 5 * (1 - rho[i])^3, foo, eachindex(rho))
+        ThreadsX.map!(i -> (1 - rho[i])^3 - 4 * (f[i] - 1) / (1 - rho[i]), foo, eachindex(rho))
         return foo
     end
 
